b82d38148377e5043a4652e800774d40066e50e4
madhavsharma2106 <madhavsharma2106@gmail.com>
2022-09-19 08:48:31 +0200

    logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,5 +1,7 @@
+const os = require("os");
 import { config } from ".";
 import * as winston from "winston";
+require("winston-syslog");
 
 const enumerateErrorFormat = winston.format((info) => {
   if (info instanceof Error) {
@@ -7,6 +9,20 @@ const enumerateErrorFormat = winston.format((info) => {
   }
   return info;
 });
+
+// @ts-ignore next-line
+const papertrail = new winston.transports.Syslog({
+  host: "logs2.papertrailapp.com",
+  port: 12106,
+  protocol: "tls4",
+  localhost: os.hostname(),
+  eol: "\n",
+});
+
+const transports = [];
+if (config.env === "development")
+  transports.push(new winston.transports.Console({}));
+if (config.env === "production") transports.push(papertrail);
 
 const _logger = winston.createLogger({
   level: config.env === "development" ? "debug" : "info",
@@ -26,7 +42,7 @@ const _logger = winston.createLogger({
       }
     )
   ),
-  transports: [new winston.transports.Console({})],
+  transports: transports, // TODO: Update to run PT on prod later.
 });
 
 export const logger = function (moduleName) {
